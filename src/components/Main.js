b9ebe8fc4a0cd86b44756c1c7f134a8a92f45686
Al-dasouqi <aldasouqif@gmail.com>
2021-06-08 17:46:07 +0300

    lab3 done!

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import HornedBeast from './HornedBeast'
-import data from './data.json'
-
-
+import CardColumns from 'react-bootstrap/CardColumns'
 
 class Main extends React.Component {
+
     render() {
-        return (
-            <>
-                {data.map(item => {
-                    return (
-                        <HornedBeast
-                            title={item.title}
-                            imgURL={item.image_url}
-                            description={item.description}
-                        />
-                    )
-                })}
-            </>
-        )
+      return (
+        <CardColumns>
+        <div>
+          {this.props.data.map(item => {
+            return (
+              <HornedBeast
+                title={item.title}
+                imgURL={item.image_url}
+                description={item.description}
+              />
+            )
+          })}
+        </div>
+        </CardColumns>
+      )
     }
-
-
-}
-export default Main;
+  }
+  export default Main;
